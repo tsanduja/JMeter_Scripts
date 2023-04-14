3a06c814b5cd6721e1e5014486e56685de73e063
tsanduja <tanishq.sanduja@soprasteria.com>
2023-04-14 16:02:21 +0530

    Git Pull File

---
+++ git_pull.bat
@@ -0,0 +1,5 @@
+@echo off
+
+echo git clone https://gitlab.ptx.fr.sopra/dassault_si_flotte_rafale/performance/aerowebb.git
+
+git clone https://gitlab.ptx.fr.sopra/dassault_si_flotte_rafale/performance/aerowebb.git
